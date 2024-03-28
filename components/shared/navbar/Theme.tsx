f68721a643eab916df160ac809c40f81ba739808
Ayesha <ayeshasolanki151@gmail.com>
2024-03-28 06:08:00 +0530

    Implemented Navbar

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -1,32 +1,66 @@
-'use client'
+"use client";
 
 import React from "react";
-// import { useTheme } from "@/context/ThemeProvider";
+import { useTheme } from "@/context/ThemeProvider";
+import Image from "next/image";
 import {
   Menubar,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
+import { themes } from "@/constants";
 
 const Theme = () => {
-  // const { mode, setMode } = useTheme();
+  const { mode, setMode } = useTheme();
   return (
-    <Menubar>
+    <Menubar className="bg:transparent relative border-none shadow-none">
       <MenubarMenu>
-        <MenubarTrigger>File</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>New Window</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Share</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Print</MenubarItem>
+        <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200 ">
+          {mode === "light" ? (
+            <Image
+              src="/assets/icons/sun.svg"
+              alt="sun"
+              width={20}
+              height={20}
+              className="active-theme"
+            />
+          ) : (
+            <Image
+              src="/assets/icons/moon.svg"
+              alt="sun"
+              width={20}
+              height={20}
+              className="active-theme"
+            />
+          )}
+        </MenubarTrigger>
+        <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
+          {themes.map((theme) => (
+            <MenubarItem
+              key={theme.value}
+              onClick={() => {
+                setMode(theme.value);
+                if(theme.value !== 'system') {
+                  localStorage.theme = theme.value;
+                }
+                else {
+                  localStorage.removeItem('theme');
+                }
+              }}
+              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400 "
+            >
+              <Image
+                src={theme.icon}
+                alt={theme.value}
+                width={16}
+                height={16}
+                className={`${mode === "theme.value" && "active-theme"} `}
+              />
+              <p className={`body-semibold text-light-500 ${mode === theme.value ? 'text-primary-500' : 'text-dark100_light900'}`}>{theme.label}</p>
+            </MenubarItem>
+          ))}
         </MenubarContent>
       </MenubarMenu>
     </Menubar>
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,46 +1,47 @@
-"use client"
-import React, {createContext, useContext, useState, useEffect} from 'react'
-
+"use client";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface ThemeContextType {
-  mode: string,
-  setMode: (mode: string) => void
+  mode: string;
+  setMode: (mode: string) => void;
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
-export function ThemeProvider({children} : {children: React.ReactNode}) {
-  const [mode, setMode] = useState('')
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if(mode === 'dark') {
-      setMode('light')
-      document.documentElement.classList.add('light')
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      // this is to checkout if the theme is dark or the theme as a key exists in localStorage and if it doesn't that means our current theme is system and if the user's OS suppots dark mode then we are explicitly setting the system theme as dark 
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove('dark');
     }
-    else {
-      setMode('dark')
-      document.documentElement.classList.add('dark')
-    }
-  }
+  };
 
-  useEffect( () => {
-    
-    handleThemeChange()
-  }, [mode, handleThemeChange])
+  useEffect(() => {
+    console.log("I am running");
+    handleThemeChange();
+  }, [mode]);
 
   return (
-    <ThemeContext.Provider value={{mode,setMode}}>
+    <ThemeContext.Provider value={{ mode, setMode }}>
       {children}
     </ThemeContext.Provider>
-  )
-} 
-
-export function useTheme() {
-  const context = useContext(ThemeContext)
-  if(context === undefined) {
-    throw new Error('useTheme must be used within a ThemeProvider')
-  }
-  return context
+  );
 }
 
-  
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+}
