054c12007e7a1cce3cc1ab2ffe0574ae6b413085
Ayesha <ayeshasolanki151@gmail.com>
2024-04-02 10:55:02 +0530

    fixed bugs

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -63,6 +63,8 @@ export async function deleteUser(params: DeleteUserParams) {
     // delete user from database: will lead to delete his/her questions, answers, comments etc.
 
     // get user question ids
+    
+    // eslint-disable-next-line no-unused-vars
     const userQuestionIds = await Question.find({ author: user._id }).distinct(
       "_id"
     );
